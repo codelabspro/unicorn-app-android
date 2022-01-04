02c41b9e52ba1ab8aa42d08b65ef7dc689d8e86d
Arunabh Das <arunabhdas@gmail.com>
2022-01-04 13:17:31 -0500

    Added viewBinding true under buildFeatures to app build.gradle

--- UnicornApp/app/src/main/java/com/codesdk/unicornapp/MainActivity.kt
+++ UnicornApp/app/src/main/java/com/codesdk/unicornapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.codesdk.unicornapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codesdk.unicornapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
