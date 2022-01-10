b82d51db44791ac3ddd2eae5c93d253af0e500b6
Arunabh Das <arunabhdas@gmail.com>
2022-01-10 17:05:20 -0500

    Improvements to MainActivity layout

--- UnicornJetpack/app/src/main/java/com/codesdk/unicornjetpack/MainActivity.kt
+++ UnicornJetpack/app/src/main/java/com/codesdk/unicornjetpack/MainActivity.kt
@@ -3,10 +3,18 @@ package com.codesdk.unicornjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.codesdk.unicornjetpack.ui.theme.UnicornJetpackTheme
 
@@ -14,11 +22,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UnicornJetpackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Unicorn")
+                Text("Jetpack")
             }
         }
     }
@@ -32,7 +44,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    UnicornJetpackTheme {
         Greeting("Android")
-    }
 }
