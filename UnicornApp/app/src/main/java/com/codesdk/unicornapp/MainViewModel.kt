a6bdf29e8bdf67973ccee3d600aa73a261a0d0c5
Arunabh Das <arunabhdas@gmail.com>
2022-01-10 15:09:13 -0500

    Bootstrap empty compose project UnicornJetpack

--- UnicornApp/app/src/main/java/com/codesdk/unicornapp/MainViewModel.kt
+++ UnicornApp/app/src/main/java/com/codesdk/unicornapp/MainViewModel.kt
@@ -1,12 +1,27 @@
 package com.codesdk.unicornapp
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class MainViewModel: ViewModel() {
     private val _loginState = MutableStateFlow<LoginUiState>(LoginUiState.Empty)
+    val loginUiState: StateFlow<LoginUiState> = _loginState
+
+    fun login(username: String, password: String) = viewModelScope.launch {
+        _loginState.value = LoginUiState.Loading
+        delay(2000L)
+        if (username == "android" && password == "secretpassword") {
+            _loginState.value = LoginUiState.Success
+        } else {
+            _loginState.value = LoginUiState.Error("Wrong credentials")
+        }
+    }
 
     sealed class LoginUiState {
         object Success: LoginUiState()
