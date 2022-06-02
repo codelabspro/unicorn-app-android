577046acbe4bb9202203a5f17f1493146e3691e5
Arunabh Das <arunabhdas@gmail.com>
2022-06-02 14:32:21 -0400

    General cleanup

--- UnicornJetpack/app/src/main/java/com/codesdk/unicornjetpack/MainActivity.kt
+++ UnicornJetpack/app/src/main/java/com/codesdk/unicornjetpack/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,21 +13,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codesdk.unicornjetpack.ui.theme.UnicornJetpackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            Row(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .padding(16.dp)
             ) {
                 Text("Unicorn")
                 Text("Jetpack")
+                Text("Compose")
             }
         }
     }
