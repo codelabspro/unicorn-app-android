166c7b72af75d072697adb93aecbb09331f0aa16
Arunabh Das <arunabhdas@gmail.com>
2023-02-08 14:12:18 -0500

    Upgrade AGP to 8.1.0-alpha03 for Android Studio Giraffe | 2022.3.1 Canary 3

--- UnicornApp-src/app/src/main/java/com/unicornapp/unicornapp/ui/theme/Theme.kt
+++ UnicornApp-src/app/src/main/java/com/unicornapp/unicornapp/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -22,19 +23,17 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF753A88),
+    secondary = Color(0xFF516395),
+    tertiary = Color(0xFF185A9D),
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+    background = Color(0xFFAA076B),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
